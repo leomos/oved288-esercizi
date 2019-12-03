f2255ca4ddb591b49b65193eecb5ad940c39b011
Leonardo Mosciatti <leonardo.mosciatti@gmail.com>
2019-12-03 09:53:28 +0100

    First version of duplicati.

---
+++ src/esercizi/duplicati/Main.java
@@ -0,0 +1,41 @@
+package esercizi.duplicati;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("/home/leonardo/work/etl/corsi/oved288/workspace/esercizi/src/esercizi/duplicati/prova.txt"));
+			
+			String line = null;
+			List<String> undup = new ArrayList<>();
+			
+			// leggo il file riga per riga
+			do {
+				line = reader.readLine();
+				
+				if(line != null) {
+					if(undup.indexOf(line) == -1) {
+						undup.add(line);
+					}
+				}
+			} while(line != null);
+			
+			// stampo tutto ciò che è dentro la lista
+			for(String s : undup) {
+				System.out.println(s);
+			}
+			
+			reader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
